afee4ae876a7b110833270f56799746f55131aa2
Rodrigo Madeiro <rodrigomadeiro@Rodrigos-MacBook-Air.local>
2024-04-24 16:11:18 -0300

    add default layout

---
+++ src/app/components/input-default/input-default.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-default',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-default.component.html',
+  styleUrl: './input-default.component.scss'
+})
+export class InputDefaultComponent {
+
+}
